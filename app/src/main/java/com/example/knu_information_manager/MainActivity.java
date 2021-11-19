99c750f711bce646f51810b83b5ab35c57cc4007
박원종 <wonjong1030@naver.com>
2021-11-19 17:11:46 +0900

    키워드 추출 기능구현

--- app/src/main/java/com/example/knu_information_manager/MainActivity.java
+++ app/src/main/java/com/example/knu_information_manager/MainActivity.java
@@ -6,13 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,17 +28,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView1);
         final Bundle bundle = new Bundle();
+        List<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         new Thread(){
             @Override
             public void run() {
-                String URL ="https://www.kongju.ac.kr/kor/article/student_news/?mno=&pageIndex=1&categoryCnt=1&searchCategory=&searchCategory0=&searchCondition=1&searchKeyword=%EB%B3%B5%EC%88%98%EC%A0%84%EA%B3%B5#article";
+                String URL ="https://www.kongju.ac.kr/kor/article/student_news/?mno=&pageIndex=1&categoryCnt=1&searchCategory=&searchCategory0=&searchCondition=1&searchKeyword=#article";
 
                 try {
                     Document doc = Jsoup.connect(URL).get();
                     Elements temele = doc.select(".subject");
                     Boolean isEmpty = temele.isEmpty();
+
+
                     if(isEmpty==false){
-                        bundle.putString("article", temele.text());
+                        for(Element e:temele){
+                            if(e.text().contains("예산")){
+                                result.add(e.text());
+                                sb.append(e.text());
+                            }
+                        }
+                        bundle.putString("article", sb.toString());
+                        System.out.println(sb.toString());
                         Message msg = handler.obtainMessage();
                         msg.setData(bundle);
                         handler.sendMessage(msg);
